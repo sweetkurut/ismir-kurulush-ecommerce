daf12ea6a88dbd0947317a63491f26ab270e02c4
sweetkurut <devxwc@gmail.com>
2025-10-13 15:39:06 +0600

    add tabs in profile page

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
 import s from "./style.module.scss";
 
 export const ProfilePage = () => {
-    return <div>ProfilePage</div>;
+    const [activeTab, setActiveTab] = useState("profile");
+
+    return (
+        <div className={s.wrapper}>
+            <div className={s.container}>
+                <div className={s.tabs}>
+                    <div
+                        className={`${s.tab} ${activeTab === "profile" ? s.active : ""}`}
+                        onClick={() => setActiveTab("profile")}
+                    >
+                        Профиль
+                    </div>
+                    <div
+                        className={`${s.tab} ${activeTab === "orders" ? s.active : ""}`}
+                        onClick={() => setActiveTab("orders")}
+                    >
+                        Заказы
+                    </div>
+                    <div
+                        className={`${s.tab} ${activeTab === "favorites" ? s.active : ""}`}
+                        onClick={() => setActiveTab("favorites")}
+                    >
+                        Избранное
+                    </div>
+                    <div
+                        className={`${s.tab} ${activeTab === "applications" ? s.active : ""}`}
+                        onClick={() => setActiveTab("applications")}
+                    >
+                        Заявки
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
